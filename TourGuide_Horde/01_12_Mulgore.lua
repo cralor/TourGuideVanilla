6bac7ae582d534a324efdf5369b08af657915a0c
cralor <cralor@gmail.com>
2018-04-08 13:19:47 -0500

    Updates to Mulgore and Tirisfal

--- TourGuide_Horde/01_12_Mulgore.lua
+++ TourGuide_Horde/01_12_Mulgore.lua
@@ -1,5 +1,4 @@
 TourGuide:RegisterGuide("Mulgore (1-12)", "The Barrens (12-15)", "Horde", function()
-
 return [[
 A The Hunt Begins |QID|747| |N|Right in front of you! (44.88, 77.18)|
 C The Hunt Begins |QID|747|
@@ -44,65 +43,67 @@ T Attack on Camp Narache |QID|781|
 
 A A Task Unfinished |QID|1656| |N|Follow the road out of town (38.51, 81.62).|
 
+A Dangers of the Windfury |QID|743|
+T A Task Unfinished |QID|1656|
+h Bloodhoof Village
 T Rites of the Earthmother (Part 2) |QID|763| |N|Keep following the road to Bloodhoof Village (47.41, 60.16).|
 A Sharing the Land |QID|745|
+A Dwarven Digging |QID|746|
 A Rite of Vision (Part 1) |QID|767|
 A Poison Water |QID|748|
 A Swoop Hunting |QID|761|
-T A Task Unfinished |QID|1656|
-h Bloodhoof Village
 T Rite of Vision (Part 1) |QID|767|
 A Rite of Vision (Part 2) |QID|771|
 A Mazzranache |QID|766|
-A Dangers of the Windfury |QID|743|
-A Kyle's Gone Missing! |QID|11129| |N|North of town, by the bridge (48.27, 53.30)|
 
 N Kill Swoops... |N|Kill any Swoops you come across for "Swoop Hunting".|
-C Sharing the Land |QID|745| |N|To the south by the cliffs (54,73).|
-C Poison Water |QID|748|
-
-T Poison Water |QID|748| |N|Back at Bloodhoof Village (48.51, 60.50)|
-A Winterhoof Cleansing |QID|754|
-T Sharing the Land |QID|745|
-A Dwarven Digging |QID|746|
-N Cook your ass off! |L|30816 40| |N|If you plan on leveling cooking (why wouldn't you?) buy the skill, 40x Simple Flour and 40x Mild Spices (about 3.5s total).  Cook up bread before you cook any meat that dropped.|
-C Kyle's Gone Missing! |QID|11129| |U|33009| |N|Find the dog running around the village and feed him the meat from the plainstrider.|
-
-C Winterhoof Cleansing |QID|754| |U|5411| |N|South of town at the well (53.55, 66.42).  Use the totem to cleanse it.|
 C Rite of Vision (Part 2) |QID|771| |N|Grab the well stones at the well.  Acorns can be found under the trees all over.|
-
-T Winterhoof Cleansing |QID|754| |N|Back at Bloodhoof Village (48.50, 60.52)|
-A Thunderhorn Totem |QID|756|
-T Rite of Vision (Part 2) |QID|771|
-A Rite of Vision (Part 3) |QID|772|
-U Drink up! |U|4823|
-T Kyle's Gone Missing! |QID|11129| |N|As you pass by, don't worry you can catch up.|
-T Rite of Vision (Part 3) |QID|772| |N|Follow the vision, or just run to the cave he's leading you to (32.82, 36.15)|
-A Rite of Wisdom |QID|773|
-
-C Dwarven Digging |QID|746| |U|4702| |N|Head south to the Bael'dun Digsite and work your way back to the forge (31.37, 49.78).  By the time you're there you should have 5 picks to break.  Use them at the forge.|
-C Thunderhorn Totem |QID|756| |N|All over the plains south of Thunder Bluff.|
-
-T Dwarven Digging |QID|746| |N|Back at Bloodhoof Village (47.47, 60.22)|
-T Thunderhorn Totem |QID|756|
-A Thunderhorn Cleansing |QID|758|
-C Thunderhorn Cleansing |QID|758| |U|5415| |N|North of town at the Thunderhorn Water Well (44.53, 45.69).  You know the drill, use the totem.|
-T Thunderhorn Cleansing |QID|758| |N|Back at Bloodhoof Village (48.48, 60.39)|
-A Wildmane Totem |QID|759|
-
+C Sharing the Land |QID|745| |N|To the south by the cliffs (54,73).|
+C Dangers of the Windfury |QID|743| |N|To the east, south of the road (64,70).|
 A The Ravaged Caravan |QID|749| |N|Find Morin Cloudstalker patrolling along the road to the east.|
 T The Ravaged Caravan |QID|749| |N|On the north edge of the lake at the Ravaged Caravan (53.81, 48.29).|
 A The Ravaged Caravan |QID|751|
+C Poison Water |QID|748|
+C Mazzranache |QID|766|
+C Swoop Hunting |QID|761|
+
+T Sharing the Land |QID|745|
+T Dangers of the Windfury |QID|743|
+T Poison Water |QID|748| |N|Back at Bloodhoof Village (48.51, 60.50)|
+T Swoop Hunting |QID|761|
+A Winterhoof Cleansing |QID|754|
+T Rite of Vision (Part 2) |QID|771|
+A Rite of Vision (Part 3) |QID|772|
+T Mazzranache |QID|766|
+
 T The Ravaged Caravan |QID|751| |N|Back at Morin Cloudstalker|
 A The Venture Co. |QID|764|
 A Supervisor Fizsprocket |QID|765|
 
-C Dangers of the Windfury |QID|743| |N|To the east, south of the road (64,70).|
-C Wildmane Totem |QID|759|
+C Winterhoof Cleansing |QID|754| |U|5411| |N|South of town at the well (53.55, 66.42).  Use the totem to cleanse it.|
 
-T Wildmane Totem |QID|759| |N|Back at Bloodhoof Village (48.43, 60.37)|
-A Wildmane Cleansing |QID|760|
-T Dangers of the Windfury |QID|743|
+T Winterhoof Cleansing |QID|754| |N|Back at Bloodhoof Village (48.50, 60.52)|
+A Thunderhorn Totem |QID|756|
+
+C Dwarven Digging |QID|746| |U|4702| |N|Head south to the Bael'dun Digsite and work your way back to the forge (31.37, 49.78).  By the time you're there you should have 5 picks to break.  Use them at the forge.|
+
+T Rite of Vision (Part 3) |QID|772| |N|Follow the vision, or just run to the cave he's leading you to (32.82, 36.15)|
+A Rite of Wisdom |QID|773|
+
+A A Sacred Burial |QID|833| |N|To the east near Red Rocks Amphitheatre (59.81, 25.52)|
+C A Sacred Burial |QID|833|
+
+T Rite of Wisdom |QID|773| |N|Talk to the Ancestral Spirit at the center of the graveyard (61.42, 20.94).|
+A Journey into Thunder Bluff |QID|775|
+
+T A Sacred Burial |QID|833|
+C Thunderhorn Totem |QID|756| |N|All over the plains south of Thunder Bluff.|
+
+A The Hunter's Way |QID|861| |N|Back at Bloodhoof Village. Hearth if you can. (47.47, 60.22)|
+T Thunderhorn Totem |QID|756|
+A Thunderhorn Cleansing |QID|758|
+
+A Heeding the Call |QID|5928| |C|Druid| |N|From the druid trainer (48.51, 59.60)|
 
 A The Hunter's Path |C|Hunter| |QID|10530|
 T The Hunter's Path |C|Hunter| |QID|10530|
@@ -117,12 +118,12 @@ C Taming the Beast (Part 3) |C|Hunter| |N|East of town| |QID|6088|
 T Taming the Beast (Part 3) |C|Hunter| |QID|6088|
 A Training the Beast |C|Hunter| |QID|6089|
 
-A Heeding the Call |QID|5928| |C|Druid| |N|From the druid trainer (48.51, 59.60)|
-
-A The Hunter's Way |QID|861|
+C Thunderhorn Cleansing |QID|758| |U|5415| |N|North of town at the Thunderhorn Water Well (44.53, 45.69).  You know the drill, use the totem.|
 
 A Preparation for Ceremony |QID|744| |N|Follow the road north to Thunder Bluff (37.54, 60.15)|
 T Training the Beast |C|Hunter| |N|Over on Hunter Rise (to the south)| |QID|6089|
+T Journey into Thunder Bluff |QID|775|
+A Rites of the Earthmother (Part 3) |QID|776|
 
 T Heeding the Call |QID|5928| |C|Druid| |N|Over on Elder Rise (76.38, 27.69)|
 A Moonglade |QID|5922| |C|Druid|
@@ -134,51 +135,60 @@ A Back to Thunder Bluff |QID|5932| |C|Druid|
 T Back to Thunder Bluff |QID|5932| |C|Druid| |N|Fly back to Thunder Bluff (76.15, 27.69)|
 A Body and Heart |QID|6002| |C|Druid|
 
-C Wildmane Cleansing |QID|760| |N|Find the well north of Bluff (42.61, 14.24), use the totem on it.  Don't worry about finishing "The Venture Co.", you'll be running into them again soon.|
-A A Sacred Burial |QID|833| |N|To the east near Red Rocks Amphitheatre (59.81, 25.52)|
-T Rite of Wisdom |QID|773| |N|Talk to the Ancestral Spirit at the center of the graveyard (61.42, 20.94).|
-A Journey into Thunder Bluff |QID|775|
-C A Sacred Burial |QID|833|
-T A Sacred Burial |QID|833|
-C Preparation for Ceremony |QID|744| |N|Kill harpies along the mountains northwest of Red Rocks (55,10).  You can also find groups northwest of the well (38,8) and due west of bluff (32,25).|
-C The Hunter's Way |QID|861| |N|Killing poor little kitties... you are a BAD cowman.|
-
-T Preparation for Ceremony |QID|744| |N|Back up on Thunder Bluff (38.07, 58.95)|
-T The Hunter's Way |QID|861| |N|On Hunter Rise (61.21, 80.48)|
-A Sergra Darkthorn |QID|860|
-T Journey into Thunder Bluff |QID|775|
-A Rites of the Earthmother (Part 3) |QID|776|
-
-C Rites of the Earthmother (Part 3) |QID|776| |N|Kill Arra'chea to the east of Bluff.  If you cannot find him, he spawns at (49,19) and patrols clockwise.  He goes north to (52,10) and south to (54,35)|
-C Swoop Hunting |QID|761|
-C Mazzranache |QID|766|
-
-T Wildmane Cleansing |QID|760| |N|Back at Bloodhoof Village (48.43, 60.35)|
-A The Demon Scarred Cloak |QID|770| |O| |N|If you happened to kill Ghost Howl while you were out and about, accept this quest from the item he dropped.|
-T The Demon Scarred Cloak |QID|770| |O|
-T Mazzranache |QID|766|
-T Swoop Hunting |QID|761|
+P The Rake |C|Hunter| |N|Tame him if available, otherwise tame a Flatland Prowler (52, 18)|
 
 C Supervisor Fizsprocket |QID|765| |N|In the Venture Co. Mine to the east of the Ravaged Caravan (64.70, 43.70)|
 C The Venture Co. |QID|764|
+N Die on Purpose
+
+T Dwarven Digging |QID|746|
+T Thunderhorn Cleansing |QID|758| |N|Back at Bloodhoof Village (48.48, 60.39)|
+A Wildmane Totem |QID|759|
+
 T Supervisor Fizsprocket |QID|765| |N|Back at Morin Cloudstalker, patrolling along the road.|
 T The Venture Co. |QID|764|
+
+C Rites of the Earthmother (Part 3) |QID|776| |N|Kill Arra'chea to the east of Bluff.  If you cannot find him, he spawns at (49,19) and patrols clockwise.  He goes north to (52,10) and south to (54,35)|
+C Preparation for Ceremony |QID|744| |N|Kill harpies along the mountains northwest of Red Rocks (55,10).  You can also find groups northwest of the well (38,8) and due west of bluff (32,25).|
+C The Hunter's Way |QID|861| |N|Killing poor little kitties... you are a BAD cowman.|
+C Wildmane Totem |QID|759|
+
+A The Barrens Oases |N|From the Archdruid on Elder Rise in TB (78.61, 28.52)| |QID|886|
+T Rites of the Earthmother (Part 3) |QID|776|
+T The Hunter's Way |QID|861| |N|On Hunter Rise (61.21, 80.48)|
+A Sergra Darkthorn |QID|860|
+T Preparation for Ceremony |QID|744| |N|Back up on Thunder Bluff (38.07, 58.95)|
+
+T Wildmane Totem |QID|759| |N|Back at Bloodhoof Village. Hearth if you can. (48.43, 60.37)|
+A Wildmane Cleansing |QID|760|
+
+N Grind your way... |N|Then die on purpose. (47, 33)|
+C Wildmane Cleansing |QID|760| |N|Find the well north of Bluff (42.61, 14.24), use the totem on it.|
+N Grind your way... |N|Then die on purpose. (47, 38)|
+T Wildmane Cleansing |QID|760| |N|Back at Bloodhoof Village (48.43, 60.35)|
 
 C Body and Heart |QID|6002| |C|Druid| |U|15710| |N|On the edge of the Barrens, just as you leave Mulgore, south of the road behind the hut (42.00, 60.89).  Use the dust, kill the boomkin, talk to him.| |Z|The Barrens|
 
 A Journey to the Crossroads |QID|854| |N|Follow the road east to Camp Taurajo (44.87, 58.65).| |Z|The Barrens|
+f Get Flight Path
 
+T The Barrens Oases |QID|886| |N|North to The Crossroads (52.3, 31.9)|
+A The Forgotten Pools |QID|870|
+T Sergra Darkthorn |QID|860|
+A Plainstrider Menace |QID|844|
+A Meats to Orgrimmar |QID|6365|
+h The Crossroads
+A Raptor Thieves |QID|869|
 T Journey to the Crossroads |QID|854| |N|Follow the road east out of town, north to the Crossroads (51.52, 30.83)| |Z|The Barrens|
+A Disrupt the Attacks |QID|871|
+A Supplies for the Crossroads |QID|5041|
+A Harpy Raiders |QID|867|
+T Meats to Orgrimmar |QID|6365| |N|Skip the follow-up quest|
+A Fungal Spores |QID|848|
+A Wharfmaster Dizzywig |QID|1492|
 A A Bundle of Hides |QID|6361| |N|In the forge at the north edge of town (51.20, 29.11)| |Z|The Barrens|
-T A Bundle of Hides |QID|6361| |N|Talk to the flight master (51.51, 30.31)| |Z|The Barrens|
-A Ride to Thunder Bluff |QID|6362|
-T Ride to Thunder Bluff |QID|6362| |N|Fly to Thunder Bluff.  Turnin is at the base of the windrider totem (45.61, 55.91)|
-A Tal the Wind Rider Master |QID|6363|
-T Rites of the Earthmother (Part 3) |QID|776|
-A The Barrens Oases |N|From the Archdruid on Elder Rise (78.61, 28.52)| |QID|886|
-T Body and Heart |QID|6002| |C|Druid|
-T Tal the Wind Rider Master |QID|6363|
-A Return to Jahan |QID|6364|
-T Return to Jahan |QID|6364| |N|Fly back to the Crossroads (51.20, 29.09)| |Z|The Barrens|
+T A Bundle of Hides |QID|6361| |N|Talk to the flight master, skip the follow-up quest (51.51, 30.31)| |Z|The Barrens
+
+T Body and Heart |QID|6002| |C|Druid| |N|Back in Thunder Bluff|
 ]]
 end)
--- TourGuide_Horde/01_12_TirisfalGlades.lua
+++ TourGuide_Horde/01_12_TirisfalGlades.lua
@@ -19,8 +19,8 @@ T Tainted Scroll |C|Warlock| |QID|3099| |T|
 C The Damned |QID|376|
 C Rattling the Rattlecages |QID|3901|
 
-T The Damned |QID|376| |T|
 T Rattling the Rattlecages |QID|3901| |T|
+T The Damned |QID|376| |T|
 A Marla's Last Wish |QID|6395| |T|
 A Night Web's Hollow |QID|380| |T|
 A Scavenging Deathknell |QID|3902| |T|
@@ -28,9 +28,9 @@ A Scavenging Deathknell |QID|3902| |T|
 C Night Web's Hollow |N|Cave to the north (27.5, 58.0)| |QID|380|
 K Samuel Fipps |L|16333| |N|Take the road north, then east.  Samuel's in a little camp just south of the road (36,62)|
 C Scavenging Deathknell |QID|3902| |N|Boxes all over Deathknell|
+C Marla's Last Wish |N|Bury Samuel's remains at Marla's Grave (31.17, 65.05)| |QID|6395| |T|
 
 T Scavenging Deathknell |QID|3902| |T|
-C Marla's Last Wish |N|Bury Samuel's remains at Marla's Grave (31.17, 65.05)| |QID|6395| |T|
 T Night Web's Hollow |QID|380| |T|
 A The Scarlet Crusade |QID|381| |T|
 T Marla's Last Wish |QID|6395| |T|
@@ -47,118 +47,112 @@ A Gordo's Task |N|Along the road to Brill| |QID|5481|
 
 R Brill
 A A Putrid Task |QID|404|
+A A New Plague (Part 1) |N|In the house near the herbalism trainer, this quest requires level 6.| |QID|367|
 T Vital Intelligence |QID|383|
 A At War With The Scarlet Crusade (Part 1) |QID|427| |NODEBUG|
+A Wanted: Maggot Eye |QID|398|
+A Graverobbers |QID|358|
 T A Rogue's Deal (Part 1) |QID|8|
 h Gallows' End Tavern
-A A New Plague (Part 1)|N|In the house near the herbalism trainer, this quest requires level 6.| |QID|367|
 
-A A Rogue's Deal (Part 2) |N|Back in Deathknell (38.27, 56.63)| |QID|590|
-C A Rogue's Deal (Part 2) |QID|590|
-T A Rogue's Deal (Part 2) |QID|590|
+C Gordo's Task |QID|5481| |N|Pick weeds, all over the area between Deathknell and Brill.|
+C A Putrid Task |QID|404| |N|Kill rotting dead and ravaged corpses west of Brill.|
+
+T A Putrid Task |QID|404|
+A The Mills Overrun |QID|426|
+T Gordo's Task |QID|5481| |N|Wandering around in the graveyard|
+A Doom Weed |QID|5482|
+
+C Graverobbers |QID|358| |N|Mongrels are at Garren's Haunt, graverobbers to the south.|
+C Doom Weed |QID|5482|
+C Wanted: Maggot Eye |N|In the house at the north end of Garren's Haunt (58.7, 31.7)| |QID|398|
+C A New Plague (Part 1) |QID|367| |N|You can find a bunch on the hill southwest of Brill.|
+
+T Wanted: Maggot Eye |QID|398|
+T Graverobbers |QID|358|
+A Forsaken Duties |QID|359|
+A The Prodigal Lich |QID|405|
+T A New Plague (Part 1) |QID|367|
+A A New Plague (Part 2) |QID|368|
+T Doom Weed |QID|5482|
 
 C Fields of Grief (Part 1) |N|At the farm north of Deathknell (35,50)| |QID|365|
 C At War With The Scarlet Crusade (Part 1) |QID|427| |NODEBUG| |N|West of the farm.|
-C Gordo's Task |QID|5481| |N|Pick weeds, all over the area between Deathknell and Brill.|
-C A Putrid Task |QID|404| |N|Kill rotting dead and ravaged corpses west of Brill.|
-C A New Plague (Part 1) |QID|367| |N|You can find a bunch on the hill southwest of Brill.|
+C A New Plague (Part 2) |QID|368| |N|Along the shore (60,29).  Murlocs.... joy.|
 
 H Brill |SZ|Gallows' End Tavern|
 A Deaths in the Family |QID|354|
 A The Haunted Mills |QID|362|
 A The Chill of Death |QID|375| |N|Upstairs in the inn (61.89, 52.65)|
-B [Coarse Thread] |N|From the trade supplier outside (61.0, 52.5)|
 T At War With The Scarlet Crusade (Part 1) |QID|427| |NODEBUG|
 A At War With The Scarlet Crusade (Part 2) |QID|370| |NODEBUG|
+A Proof of Demise |QID|374|
 T Fields of Grief (Part 1) |QID|365|
 A Fields of Grief (Part 2) |QID|407|
-T A New Plague (Part 1) |QID|367|
-A A New Plague (Part 2) |QID|368|
-T A Putrid Task |QID|404|
-A The Mills Overrun |QID|426|
-T Gordo's Task |QID|5481| |N|Wandering around in the graveyard|
-A Doom Weed |QID|5482|
-A Proof of Demise |QID|374|
-A Graverobbers |QID|358|
-A Wanted: Maggot Eye |QID|398|
-T Fields of Grief (Part 2) |QID|407| |N|Downstairs in the inn|
-
-C The Chill Of Death |QID|375| |N|Work your way north from town killing greater duskbats near the road.|
-C A New Plague (Part 2) |QID|368| |N|Along the shore (60,29).  Murlocs.... joy.|
-C Wanted: Maggot Eye |N|In the house at the north end of Garren's Haunt (58.7, 31.7)| |QID|398|
-C Graverobbers |QID|358| |N|Mongrels are at Garren's Haunt, graverobbers to the south.|
-C Doom Weed |QID|5482|
-
-T Doom Weed |QID|5482|
 T A New Plague (Part 2) |QID|368|
 A A New Plague (Part 3) |QID|369|
-T Wanted: Maggot Eye |QID|398|
-T Graverobbers |QID|358|
-A Forsaken Duties |QID|359|
-A The Prodigal Lich |QID|405|
-T The Chill of Death |QID|375|
 
 C The Mills Overrun |N|At Agamand Mills| |QID|426|
-C The Haunted Mills |QID|362|
-C Deaths in the Family |QID|354|
+C The Haunted Mills |QID|362| |N|He is at (47.34, 40.78)|
+C Deaths in the Family |QID|354| |N|These are at (49.74,36.34) (46.74,29.31) (44,33.64)|
 
-H Brill |SZ|Gallows' End Tavern|
+N Die on Purpose
+T The Mills Overrun |QID|426|
+A A Letter Undelivered |U|2839| |O| |QID|361|
 T Deaths in the Family |QID|354|
 T The Haunted Mills |QID|362|
 A Speak with Sevren |QID|355|
-A A Letter Undelivered |U|2839| |O| |QID|361|
 T A Letter Undelivered |O| |QID|361|
+T Fields of Grief (Part 2) |QID|407| |N|Downstairs in the inn|
+
+A Halgar's Summons |C|Warlock| |QID|1478|
+
 T Speak with Sevren |QID|355|
 A The Family Crypt |QID|408|
-T The Mills Overrun |QID|426|
+C The Chill Of Death |QID|375| |N|Work your way north from town killing greater duskbats near the road.|
+B Coarse Thread |U|2320| |N|Just one is required|
+T The Chill of Death |QID|375|
 
+R Undercity
+T Halgar's Summons |C|Warlock| |QID|1478|
+A Creature of the Void |C|Warlock| |QID|1473|
+T The Prodigal Lich |N|In the magic quarter| |QID|405|
+
+C At War With The Scarlet Crusade (Part 2) |N|Leave Undercity through the sewers and you'll end up near the mobs for this quest.| |QID|370| |NODEBUG|
+C Creature of the Void |C|Warlock| |N|Loot the chest at (51,67)| |QID|1473|
+T Creature of the Void |C|Warlock| |QID|1473|
+A The Binding |C|Warlock| |QID|1471|
+C The Binding |C|Warlock| |QID|1471|
+T The Binding |C|Warlock| |QID|1471|
+
+T At War With The Scarlet Crusade (Part 2) |N|Back in Brill. Hearth if you can.| |QID|370| |NODEBUG|
+A At War With The Scarlet Crusade (Part 3) |QID|371| |NODEBUG|
 T Forsaken Duties |N|Southeast of Brill at the campfire (65.5, 60.0)| |QID|359|
 A Return to the Magistrate |QID|360|
 A Rear Guard Patrol |QID|356|
 C Rear Guard Patrol |N|At the farm further east down the road (75,60).| |QID|356|
-T Rear Guard Patrol |QID|356|
-T Return to the Magistrate |QID|360|
-
-R Undercity
-T The Prodigal Lich |N|In the magic quarter| |QID|405|
-A The Lich's Identity |QID|357|
-A The Deathstalkers |C|Rogue| |QID|1978|
-
-C At War With The Scarlet Crusade (Part 2) |N|Leave Undercity through the sewers and you'll end up near the mobs for this quest.| |QID|370| |NODEBUG|
-C Proof of Demise |QID|374|
-C The Deathstalkers |C|Rogue| |QID|1978|
-
-T At War With The Scarlet Crusade (Part 2) |N|Back in Brill| |QID|370| |NODEBUG|
-A At War With The Scarlet Crusade (Part 3) |QID|371| |NODEBUG|
-T Proof of Demise |QID|374|
-
-C The Lich's Identity |N|Book on the island (67.93, 42.15)| |QID|357|
 C At War With The Scarlet Crusade (Part 3) |QID|371| |NODEBUG|
 C A New Plague (Part 3) |QID|369|
 
-H Brill |SZ|Gallows' End Tavern|
+T Rear Guard Patrol |QID|356|
 T At War With The Scarlet Crusade (Part 3) |QID|371| |NODEBUG|
 A At War With The Scarlet Crusade (Part 4) |QID|372| |NODEBUG|
 T A New Plague (Part 3) |QID|369|
 A A New Plague (Part 4) |QID|492|
-T A New Plague (Part 4) |QID|492|
+A Delivery to Silverpine Forest |QID|445|
 
-T The Lich's Identity |N|Back in Undercity| |QID|357|
-A Return the Book |QID|366|
-T The Deathstalkers |C|Rogue| |QID|1978|
-T Return the Book |N|Back on the island (68.18, 41.98).| |QID|366|
-A Proving Allegiance |QID|409|
-C Proving Allegiance |QID|409| |N|Grab a candle from the crate, then summon Lillith at the table on the island to the south (66.64, 44.82).|
-T Proving Allegiance |QID|409|
-A The Prodigal Lich Returns |QID|411|
-C At War With The Scarlet Crusade (Part 4) |QID|372| |NODEBUG|
-
-T At War With The Scarlet Crusade (Part 4) |N|Back in Brill| |QID|372| |NODEBUG|
 C The Family Crypt |N|At Agamand Mills| |QID|408|
-T The Family Crypt |QID|408|
-T The Prodigal Lich Returns |N|Back in Undercity| |QID|411|
 
-F Orgrimmar |N|Take the zepplin outside Undercity|
+N Die on Purpose
+T Return to the Magistrate |QID|360|
+T The Family Crypt |QID|408|
+T A New Plague (Part 4) |QID|492|
+C At War With The Scarlet Crusade (Part 4) |QID|372| |N|You can find him at (79.52,25.15)| |NODEBUG|
+C Proof of Demise |QID|374|
+
+T At War With The Scarlet Crusade (Part 4) |N|Back in Brill. Hearth if you can.| |QID|372| |NODEBUG|
+T Proof of Demise |QID|374|
+
+F Orgrimmar |N|Take the zeppelin outside Undercity|
 f Grab flight point
-]]
-end)
+]] end)
