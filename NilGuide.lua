e0dbbf27f565014f94d629ebbae6a1e13a26e797
cralor <cralor@gmail.com>
2018-06-11 19:40:06 -0500

    TOC cleanup and initial addon load error fixed

--- NilGuide.lua
+++ NilGuide.lua
@@ -1,2 +1,4 @@
+local L = TourGuide.Locale
+
 TourGuide:RegisterGuide("No Guide", nil, "Alliance", function() return L["K No guide loaded... |N|Click to select a guide|"] end)
 TourGuide:RegisterGuide("No Guide", nil, "Horde", function() return L["K No guide loaded... |N|Click to select a guide|"] end)
