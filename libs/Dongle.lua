377dec49bfeac013e81533aed7dfbb70871c287e
cralor <cralor@users.noreply.github.com>
2018-06-21 14:52:02 -0500

    Dongle: missed a rename

--- libs/Dongle.lua
+++ libs/Dongle.lua
@@ -514,7 +514,7 @@ local function printHelp(obj, method, header, frame, msg, a1,a2,a3,a4,a5,a6,a7,a
 		msg = "|cFF33FF99"..name.."|r: "..tostring(msg)
 	end
 
-	if TourGuide.select("#", a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20) > 0 then
+	if TG.select("#", a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20) > 0 then
 		msg = TG.join(", ", {msg, argsToStrings(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20)})
 	end
 
