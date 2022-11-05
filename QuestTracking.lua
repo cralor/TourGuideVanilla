b5bc23f7054d2193887c5df40ccd655c17823c01
LaYt <laytya@gmail.com>
2022-11-05 10:54:50 +0700

    cleanup debug

--- QuestTracking.lua
+++ QuestTracking.lua
@@ -141,7 +141,7 @@ end
 local orig = GetQuestReward
 GetQuestReward = function(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20)
 	local quest = string.gsub(GetTitleText(), "%[[0-9%+%-]+]%s", "")
-	printT({"GetQuestReward", GetTitleText(), quest} )
+	
 	TourGuide:Debug( "GetQuestReward", quest)
 	TourGuide:CompleteQuest(quest, true)
 
